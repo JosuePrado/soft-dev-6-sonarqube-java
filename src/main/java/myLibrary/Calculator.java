7382d20e0ff0b0dfd6ef34ba3b37e41aeb1e85f7
JosuePrado <prado69508698@gmail.com>
2024-10-01 20:40:08 -0400

    feat: implement calculator and his test

---
+++ src/main/java/myLibrary/Calculator.java
@@ -0,0 +1,19 @@
+package myLibrary;
+
+public class Calculator {
+    public static int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int Rest(int a, int b) {
+        return a - b;
+    }
+
+    public static int Mult(int a, int b) {
+        return a * b;
+    }
+
+    public static int Div(int a, int b) {
+        return a / b;
+    }
+}
